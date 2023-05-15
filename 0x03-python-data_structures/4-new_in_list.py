ccc7d8ec372605854cf01302ebda6cf6a8b59383
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-15 06:34:22 -0700

    bug fix in comparison logic

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if isinstance(my_list, list):
         new_list = my_list.copy()
-        if 0 >= idx < len(new_list)
+        if 0 <= idx < len(new_list)
             new_list[idx] = element
         return new_list
