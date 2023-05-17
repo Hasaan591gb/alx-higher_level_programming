b69c9d6ee785312a76aa5d9154748a954c47d0ef
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-17 12:39:48 -0700

    implemented zip to reduce code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = a_dictionary.copy()
-    for key in new_dictionary:
-        new_dictionary[key] *=  2
-    return new_dictionary
+    return dict(zip(a_dictionary.keys()
+                map(lambda value: value * 2, a_dictionary.values())))
