6d067b8371654f985434d9ebcecdeb9097b6f875
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-29 18:27:13 -0700

    added functionality for type check of PyListObject in print_python_list function

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -80,11 +80,16 @@ void print_python_list(PyObject *p)
 	Py_ssize_t size, i;
 	PyListObject *list = (PyListObject *)p;
 
-	size = list->ob_base.ob_size;
-
 	fflush(stdout);
 
 	printf("[*] Python list info\n");
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	size = list->ob_base.ob_size;
 	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", list->allocated);
 
