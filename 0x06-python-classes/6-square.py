b55974047b416407e16fd06fce60cb6e9c0ad322
Hasaan591gb <ahmad591gb@gmail.com>
2023-05-30 17:20:14 -0700

    removee error check in __init__

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -16,11 +16,6 @@ class Square:
         :raises ValueError: If size is less than 0.
         """
 
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
         self.size = size
         self.position = position
 
