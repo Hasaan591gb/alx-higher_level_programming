d6bc00184c3a4bccc51d9ad501f7f67b5cee8cb6
Hasaan591gb <ahmad591gb@gmail.com>
2023-06-12 15:36:38 -0700

    defines a new square object based on rectangle object

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module that defines class Square based on Rectagle class"""
+
+
+class Square(__import__('9-rectangle').Rectangle):
+    """Defines a square object"""
+
+    def __init__(self, size):
+        """Initialize a new square" size"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
